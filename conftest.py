356540b3525741a320cb37df894b988fac0d4aea
agulyayko <agulyayko@kzn.beeline.ru>
2020-11-29 16:12:16 +0400

    selenoid options added

--- conftest.py
+++ conftest.py
@@ -7,9 +7,14 @@ from environment.config import GetConfig as config
 
 @pytest.fixture(scope="module")
 def driver(request):
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
+    capabilities = {
+        "browserName": "chrome",
+        "enableVNC": True,
+        "enableVideo": False
+    }
+    driver = webdriver.Remote(command_executor="http://34.107.8.158:4444/wd/hub", desired_capabilities=capabilities)
     driver.maximize_window()
-    # driver = webdriver.Remote(command_executor=command_executor, desired_capabilities=capabilities)
     server_url = config().get('environment', 'host')
     driver.get(server_url)
     request.addfinalizer(driver.quit)
